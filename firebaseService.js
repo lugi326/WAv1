77a45a81695b350a9549f556b5584c69031faa69
lugi326 <lugyud088@gmail.com>
2024-07-22 08:13:42 -0700

    Update firebaseService.js

--- firebaseService.js
+++ firebaseService.js
@@ -1,10 +1,21 @@
-// firebaseService.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://dbwa-658de-default-rtdb.asia-southeast1.firebasedatabase.app/' // Ganti dengan URL database Anda
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.database();
